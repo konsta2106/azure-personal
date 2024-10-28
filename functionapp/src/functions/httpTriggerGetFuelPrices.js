0aaed97f90540a893cf664af8e11042b039eb118
Konstantin Komovits <konsta.2106@gmail.com>
2024-10-28 19:14:07 +0200

    Add http function

---
+++ functionapp/src/functions/httpTriggerGetFuelPrices.js
@@ -0,0 +1,33 @@
+const { app, input } = require('@azure/functions');
+
+const cosmosInput = input.cosmosDB({
+    databaseName: process.env["DATABASE_NAME"],
+    containerName: process.env["FUEL_CONTAINER_NAME"],
+    connection: 'MyAccount_COSMOSDB',
+});
+
+app.http('httpTriggerGetFuelPrices', {
+    methods: ['GET'],
+    authLevel: 'function',
+    extraInputs: [cosmosInput],
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const fuelData = context.extraInputs.get(cosmosInput)
+
+        context.log('Fuel data:', fuelData)
+
+        const cleanedData = fuelData.map(doc => {
+            const { _rid, _self, _etag, _attachments, _ts, ...cleanedDoc } = doc;
+            return cleanedDoc;
+        });
+
+        return {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cleanedData)
+        };
+    }
+});
