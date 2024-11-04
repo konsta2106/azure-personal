d9a077e4ed1dad26fb5c1417d9e20c10b2dbd163
Konstantin Komovits <konsta.2106@gmail.com>
2024-11-04 20:36:22 +0200

    Add new http function, remove stations data to be stored on db

--- functionapp/src/functions/httpTriggerGetLatestFuelPrices.js
+++ functionapp/src/functions/httpTriggerGetLatestFuelPrices.js
@@ -1,32 +1,26 @@
-const { app, output } = require('@azure/functions');
+const { app } = require('@azure/functions');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const cosmosOutput = output.cosmosDB({
-    databaseName: process.env["DATABASE_NAME"],
-    containerName: process.env["FUEL_CONTAINER_NAME"],
-    connection: 'MyAccount_COSMOSDB',
-});
-
-app.timer('timerTriggerGetFuelPricesToDb', {
-    schedule: '0 0 17 * * *',  // Runs every day at 7 AM UTC
-    return: cosmosOutput,
-    handler: async (myTimer, context) => {
-        context.log('Timer function executed at: ', new Date().toISOString());
+app.http('httpTriggerGetLatestFuelPrices', {
+    methods: ['GET', 'POST'],
+    authLevel: 'function',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
 
         try {
             // Fetching the HTML content of the page
             const { data } = await axios.get('https://www.tankille.fi/tampere/');
-            
+
             // Loading HTML into cheerio for scraping
             const $ = cheerio.load(data);
-            
+
             // Array to hold fuel prices and stations
             const fuel95Stations = [];
-            
+
             // Extracting the "Keskiarvo" (average price)
             const keskiarvo = $('.tab-pane#fuel-95 h6').text().replace('Keskiarvo', '').trim();
-            
+
             // Scraping the stations table
             $('#fuel-95 table tbody tr').each((i, element) => {
                 const station = {
@@ -37,7 +31,7 @@ app.timer('timerTriggerGetFuelPricesToDb', {
                 };
                 fuel95Stations.push(station);
             });
-            
+
             // Logging the scraped data (you can also insert this into a database)
             // context.log('Stations: ', fuel95Stations);
             // context.log('Keskiarvo (average price): ', keskiarvo);
@@ -45,12 +39,19 @@ app.timer('timerTriggerGetFuelPricesToDb', {
             let dataToReturn = {
                 id: Date.now().toString(),
                 keskiarvo,
+                stations: fuel95Stations,
                 timestamp: new Date().toISOString()
             }
             context.log('Data to return:', dataToReturn);
 
             // Return data to be inserted into Cosmos DB
-            return dataToReturn;
+            return {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                jsonBody: dataToReturn
+            };
 
         } catch (error) {
             context.log('Error fetching data:', error);
